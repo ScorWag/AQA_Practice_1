5bdb023a0f6cf101db411ecf3edcc1530fe2c00e
ScorWag <scorwag@rambler.ru>
2022-07-17 22:26:07 +0300

    Add tests for CahsbackHacker

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,63 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHacker cashback = new CashbackHacker();
+
+        int amount = 900;
+        int actual = cashback.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainWithOneHundred() {
+        CashbackHacker cashback = new CashbackHacker();
+
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainWithBorderValuesMore() {
+        CashbackHacker cashback = new CashbackHacker();
+
+        int amount = 1001;
+        int actual = cashback.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainWithBorderValuesLess() {
+        CashbackHacker cashback = new CashbackHacker();
+
+        int amount = 999;
+        int actual = cashback.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainWithZero() {
+        CashbackHacker cashback = new CashbackHacker();
+
+        int amount = 0;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+}
